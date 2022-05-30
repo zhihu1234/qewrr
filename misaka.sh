5feb699a165f712899073ff4441badbbd25e0c4d
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-05-31 05:00:54 +0800

    Update misaka.sh

--- misaka.sh
+++ misaka.sh
@@ -17,6 +17,7 @@ yellow() {
     echo -e "\033[33m\033[01m$1\033[0m"
 }
 
+clear
 echo "#############################################################"
 echo -e "#                ${RED} Goorm  节点一键安装脚本${PLAIN}                    #"
 echo -e "# ${GREEN}作者${PLAIN}: Misaka No                                           #"
@@ -24,3 +25,12 @@ echo -e "# ${GREEN}网址${PLAIN}: https://owo.misaka.rest                      
 echo -e "# ${GREEN}论坛${PLAIN}: https://vpsgo.co                                    #"
 echo -e "# ${GREEN}TG群${PLAIN}: https://t.me/misakanetcn                            #"
 echo "#############################################################"
+echo ""
+
+read -rp "是否安装脚本？ [Y/N]：" yesno
+
+if [[ $yesno =~ "Y"|"y" ]]; then
+
+else
+    exit 1
+fi
